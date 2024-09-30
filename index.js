60e536b754fe73363a1e78bceb7da20f4bfaae8d
John Gaitho <jgaitho016@gmail.com>
2024-09-30 12:47:28 +0300

    completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+// Code your solutions in this fileer
+function writeCards (namesArray, event) {
+    let thankYou = []
+    for (let i = 0; i < namesArray.length; i++ ) {
+      thankYou.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+    } 
+    return thankYou;
+  }
+  function countDown(initialNumber) {
+    while (initialNumber >= 0) {
+      console.log (initialNumber);
+      initialNumber -= 1;
+    }
+  }
